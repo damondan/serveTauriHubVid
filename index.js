e1243639d517ede8bc437b81a906fcb9eb833e6f
damondan <damon-r-d-1976@protonmail.com>
2026-02-10 14:49:57 -0600

    Added process.env.PORT

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve videos folder
+app.use("/videos", express.static(path.join(process.cwd(), "videos")));
+
+app.get("/", (req, res) => {
+  res.send("Video server running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
